4794f7e055ddeec13aff02bac5a804f31a7e5974
Marina <111554076+marinochkapolyakova@users.noreply.github.com>
2023-04-01 16:49:42 +0300

    Initial Commit

---
+++ VK/VKApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKApp.swift
+//  VK
+//
+//  Created by marinapolyakova on 01.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
